1569556a227e779453fccd775418acebf31873a7
Skinick <107771280+Skinick@users.noreply.github.com>
2023-05-06 00:27:47 +0300

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesElem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesElem.length}`);
+
+categoriesElem.forEach(categorie => {
+    console.log(`Category: ${categorie.firstElementChild.textContent}`);
+    console.log(`Elements: ${categorie.lastElementChild.children.length}`);
+})
